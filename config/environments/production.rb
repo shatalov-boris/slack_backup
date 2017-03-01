6f4e31889282675e91d4e9dc8323fba8e48a97dc
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-01 18:08:04 +0600

    use scss as css compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,13 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :scss
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
