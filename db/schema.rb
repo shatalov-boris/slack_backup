2de1cac3b6b48b008191d94d75f632cfa08417a6
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-01 18:47:27 +0600

    create Team

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301121949) do
+ActiveRecord::Schema.define(version: 20170301123935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "slack_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slack_id"], name: "index_teams_on_slack_id", unique: true, using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",              null: false
