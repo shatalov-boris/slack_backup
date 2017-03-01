7e55e9b449685af35350f6d6f37c8f0a813e2842
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-01 18:35:54 +0600

    create User

---
+++ db/migrate/20170301121949_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :username, null: false
+      t.string :slack_access_token, null: false
+      t.string :slack_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+  end
+end
