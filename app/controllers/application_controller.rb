9ec8c68abf658d5e434dc01408d7bc7340d0c388
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-02 13:52:46 +0600

    create route and controller for Team

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(_resource)
-    "/team"
+    team_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root "home#index"
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  resource :team, only: [:show]
 end
