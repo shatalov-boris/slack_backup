cb3b3c149b9ae6604471a009764f72dd5265342b
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-02 13:58:33 +0600

    make team page as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   default_url_options host: ENV.fetch("DOMAIN") { "localhost:3000" }
-  root "home#index"
+  root "teams#show"
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
