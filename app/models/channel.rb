6150634bae209e06789dc94f1ca802ac09fe1455
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-02 17:09:35 +0600

    add enum to Channel

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,15 @@
 class Channel < ApplicationRecord
   has_many :channel_members
   has_many :users, through: :channel_members
+
+  enum status: {
+    opened: 0,
+    archived: 1
+  }
+
+  enum channel_type: {
+    public: 0,
+    private: 1,
+    direct_message: 2
+  }
 end
