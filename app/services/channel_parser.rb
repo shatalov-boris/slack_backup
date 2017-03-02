5613c0b0e27404688088e3a7f30484fc05ac3e97
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-02 18:33:30 +0600

    refactor ChannleParserWorker

--- app/services/channel_parser.rb
+++ app/services/channel_parser.rb
@@ -1,28 +1,12 @@
 class ChannelParser
   def self.parse_all(slack_access_token)
     slack_api_client = SlackApiClient.new(slack_access_token)
-    parse(slack_api_client.public_channels, false)
 
-    slack_api_client = SlackApiClient.new(slack_access_token)
-    parse(slack_api_client.private_channels, true)
-  end
-
-  def self.parse(parsed_channels, is_private)
-    parsed_channels.each do |parsed_channel|
-      channel = Channel.find_or_initialize_by(slack_id: parsed_channel["id"])
-      channel.name = parsed_channel["name"]
-      channel.creator_slack_id = parsed_channel["creator"]
-      channel.topic = parsed_channel["topic"]["value"] if parsed_channel["topic"]
-      channel.purpose = parsed_channel["purpose"]["value"] if parsed_channel["purpose"]
-      channel.status = parsed_channel["is_archived"] ? :archived : :opened
-      channel.channel_type = is_private ? :private_channel : :public_channel
-      channel.save!
-
-      channel.channel_members.destroy_all
-      parsed_channel["members"]&.each do |member_id|
-        user = User.find_by(slack_id: member_id)
-        channel.users << user if user
-      end
+    slack_api_client.public_channels.each do |channel|
+      ChannelParserWorker.perform_async(channel, false)
+    end
+    slack_api_client.private_channels.each do |channel|
+      ChannelParserWorker.perform_async(channel, true)
     end
   end
 end
--- app/workers/channel_parser_worker.rb
+++ app/workers/channel_parser_worker.rb
@@ -1,7 +1,24 @@
 class ChannelParserWorker
   include Sidekiq::Worker
 
-  def perform(slack_access_token)
-    ChannelParser.parse_all(slack_access_token)
+  def perform(parsed_channel, is_private)
+    Rails.logger.info("[ChannelParserWorker] Started")
+
+    channel = Channel.find_or_initialize_by(slack_id: parsed_channel["id"])
+    channel.name = parsed_channel["name"]
+    channel.creator_slack_id = parsed_channel["creator"]
+    channel.topic = parsed_channel["topic"]["value"] if parsed_channel["topic"]
+    channel.purpose = parsed_channel["purpose"]["value"] if parsed_channel["purpose"]
+    channel.status = parsed_channel["is_archived"] ? :archived : :opened
+    channel.channel_type = is_private ? :private_channel : :public_channel
+    channel.save!
+
+    channel.channel_members.destroy_all
+    parsed_channel["members"]&.each do |member_id|
+      user = User.find_by(slack_id: member_id)
+      channel.users << user if user
+    end
+
+    Rails.logger.info("[ChannelParserWorker] Finished for Channel with ID = #{channel.id}")
   end
 end
