974f340a41b07bb6fcf79c2bd066b68488ba6f52
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-08 01:30:15 +0600

    handle nil situtations

--- app/services/channels_parser.rb
+++ app/services/channels_parser.rb
@@ -2,10 +2,10 @@ class ChannelsParser
   def self.parse_all(slack_access_token)
     slack_api_client = SlackApiClient.new(slack_access_token)
 
-    slack_api_client.public_channels.each do |channel|
+    slack_api_client.public_channels&.each do |channel|
       ChannelParserWorker.perform_async(channel, false)
     end
-    slack_api_client.private_channels.each do |channel|
+    slack_api_client.private_channels&.each do |channel|
       ChannelParserWorker.perform_async(channel, true)
     end
   end
--- app/services/members_parser.rb
+++ app/services/members_parser.rb
@@ -1,6 +1,6 @@
 class MembersParser
   def self.parse_all(team, slack_access_token)
-    SlackApiClient.new(slack_access_token).team_members.each do |member|
+    SlackApiClient.new(slack_access_token).team_members&.each do |member|
       MembersParserWorker.perform_async(member, team.id)
     end
   end
