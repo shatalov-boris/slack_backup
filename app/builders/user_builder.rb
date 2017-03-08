203beb6ecd2b9b86d3cafa657a0018a1fdc59357
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-08 19:27:56 +0600

    ignore bots for now

--- app/builders/user_builder.rb
+++ app/builders/user_builder.rb
@@ -12,8 +12,10 @@ class UserBuilder
     end
   end
 
-  def self.from_api(member)
+  def self.from_api(member, team_id)
+    # TODO: handle bots
     return unless member["profile"]["email"]
+
     User.find_or_initialize_by(slack_id: member["id"]) do |user|
       user.email      = member["profile"]["email"]
       user.username   = member["name"]
@@ -21,6 +23,7 @@ class UserBuilder
       user.last_name  = member["profile"]["last_name"]
       user.avatar     = member["profile"]["image_24"]
       user.password   = Devise.friendly_token[0, 20]
+      user.team_id    = team_id
     end
   end
 end
--- app/workers/members_parser_worker.rb
+++ app/workers/members_parser_worker.rb
@@ -3,8 +3,13 @@ class MembersParserWorker
 
   def perform(user_info, team_id)
     Rails.logger.info("[MembersParserWorker] Started")
-    user = UserBuilder.from_api(user_info)
-    user.team_id = team_id
+    user = UserBuilder.from_api(user_info, team_id)
+
+    unless user
+      return
+      Rails.logger.info("[MembersParserWorker] Finished")
+    end
+
     if user.new_record? || user.changed?
       user.save!
       Rails.logger.info("[MembersParserWorker] Created new user #{user.username}")
