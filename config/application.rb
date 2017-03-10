5480eacaab92257711aa1b14b21245a82265ceb2
Boris Shatalov <shatalov.bs@gmail.com>
2017-03-10 13:46:54 +0600

    set time_zone

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ Bundler.require(*Rails.groups)
 module SlackBackup
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
+    config.time_zone = ENV["TIME_ZONE"]
   end
 end
